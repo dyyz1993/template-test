20a44dce32c2f6d272bf263068158b006548d93b
xuyingzhou <xuyingzhou@tuzhanai.com>
2024-04-09 16:19:52 +0800

    feat: add

---
+++ ask.js
@@ -0,0 +1,28 @@
+module.exports = {
+    var:[
+        {
+            type: 'input',
+            message: '输入名称',
+            name: 'name',
+            default: 'zerone-app', // 默认值
+        },
+        {
+            type: 'input',
+            message: '输入名称',
+            name: 'version',
+            default: '1.0.0', // 默认值
+        },
+        {
+            type: 'input',
+            message: '输入描述',
+            name: 'description',
+            default: 'this is description', // 默认值
+        },
+        {
+            type: 'input',
+            message: 'input author',
+            name: 'author',
+            default: 'this is author', // 默认值
+        }
+    ]
+}
